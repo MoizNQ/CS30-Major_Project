5fb19135c654f42176be3e247e5806f1ee4a84ac
MoizNQ <moizsims9@gmail.com>
2022-11-29 11:17:03 -0600

    Improvement towards proposal

--- sketch.js
+++ sketch.js
@@ -5,6 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let terrain;
+let stickFigure;
+let item1, item2, item3, item4, item5;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
